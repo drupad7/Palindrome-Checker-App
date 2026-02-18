ac0bd731ed9b46002ad92666340ec52695d6864a
drupad7 <drupadgovind7@gmail.com>
2026-02-18 14:41:03 +0530

    UC1-Application Entry & Welcome Message

--- Palindrome Checker App/src/PalindromeCheckerApp.java
+++ Palindrome Checker App/src/PalindromeCheckerApp.java
@@ -1,6 +1,9 @@
 public class PalindromeCheckerApp {
 
-}
-public static void main (String[] args){
+    public static void main(String[] args) {
 
+        //use case : 1 welcome message
+        System.out.println("Welcome to Palindrome Checker APP Management system.");
+
+    }
 }
